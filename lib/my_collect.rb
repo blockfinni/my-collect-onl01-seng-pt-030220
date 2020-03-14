f0d5de2a6a45421257235770b1963f2d586c8532
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 05:41:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 
 def my_collect(collection)
   i = 0
-  new_collection = []
+  array1 = []
   while i < collection.length
-    new_collection << yield(collection[i])
+    array1 << yield(collection[i])
     i += 1
   end
-  new_collection
+  array1
 end
